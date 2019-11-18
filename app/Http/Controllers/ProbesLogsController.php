c0c031bfbac6fc79874e47c3632a16462c74481b
James Budlong <jamesbudlong03@gmail.com>
2019-11-18 16:51:35 +0800

    Updated wrong model for inserting probes logs.

--- app/Http/Controllers/ProbesLogsController.php
+++ app/Http/Controllers/ProbesLogsController.php
@@ -61,7 +61,7 @@ class ProbesLogsController extends Controller
     public function store(Request $request)
     {
 		//get probe id first based on serial number. Note: $request['probe_id'] passed is the serial number.
-		$probe = Probes::where('serial_number', $request['probe_id'])->get();
+		$probe = Probe::where('serial_number', $request['probe_id'])->get();
 		$request['probe_id'] = $probe->id;
 
         try{
